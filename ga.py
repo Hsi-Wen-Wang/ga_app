8f80da2851fd84a89513c32258543381482fbada
francis <francis7240@gmail.com>
2023-06-20 18:48:59 +0800

    machine set function version1 complete!

--- ga.py
+++ ga.py
@@ -17,10 +17,15 @@ class BaseSet:
         self.typ = typ
         self.ID = ID
 
-class Machine(BaseSet):
+class Machine:
     def __init__(self):
-        super().__init__(typ='None', ID=-1)
         self.machine_name = 'None'
+        self.processing = []
+        
+class Process:
+    def __init__(self):
+        self.typ = 'Error'
+        self.ID = '-1'
         self.time = -1
         self.cost = -1
 
@@ -30,52 +35,47 @@ class Job(BaseSet):
         self.order = -1
         self.machine = -1
 
+
 def machine_set(machine_list, machine_filename):
     with open (machine_filename, newline='') as csvfile:
         machine_data = list(csv.reader(csvfile))
         for i in range(2,len(machine_data)):
             machine_set = Machine()
             machine_set.machine_name = machine_data[i][0]
-            for j in range(len(machine_data[0])):                
+            for j in range(len(machine_data[0])):
+                process_do = Process()              
                 if machine_data[i][j] == '1' and machine_data[i][j+1] == '1' and machine_data[1][j] == ('A'):
-                    machine_set.typ = 'both'                    
-                    machine_set.ID = machine_data[0][j]
-                    machine_set.time = machine_data[i][j+2]
-                    machine_list.append(machine_set)                
+                    process_do.typ = 'both'                    
+                    process_do.ID = machine_data[0][j]
+                    process_do.time = machine_data[i][j+2]
+                    machine_set.processing.append(process_do)             
                 elif machine_data[i][j] == '1' and machine_data[i][j+1] != '1' and machine_data[1][j] == ('A'):
-                    machine_set.typ = 'A'                    
-                    machine_set.ID = machine_data[0][j]
-                    machine_set.time = machine_data[i][j+2]
-                    machine_list.append(machine_set)
+                    process_do.typ = 'A'                    
+                    process_do.ID = machine_data[0][j]
+                    process_do.time = machine_data[i][j+2]
+                    machine_set.processing.append(process_do)
                 elif machine_data[i][j] == '1' and machine_data[i][j+1] != '1' and machine_data[i][j-1] != '1' and machine_data[1][j] == ('B'):
-                    machine_set.typ = 'B'                    
-                    machine_set.ID = machine_data[0][j]
-                    machine_set.time = machine_data[i][j+1]
-                    machine_list.append(machine_set)
+                    process_do.typ = 'B'                    
+                    process_do.ID = machine_data[0][j]
+                    process_do.time = machine_data[i][j+1]
+                    machine_set.processing.append(process_do)
+            machine_list.append(machine_set)
+
     return machine_list
 
-def job_set(job_list, jobfilename):
-    with open(jobfilename, newline='') as csvfile:
-        job_data = list(csv.reader(csvfile))
-        for i in range(len(job_data)):
-            for j in range(2, len(job_data[0])):
-                job_set = Job()
-                job_set.typ = job_data[i][0]
-                job_set.order = job_data[i][1]
-                job_set.ID = job_data[i][j]
-                job_set.machine = randMachine(1,2)
-                job_list.append(job_set)
-    return job_list
-def randMachine(a, b):
-    return a+b
+
 
 if __name__ == '__main__':
     machine_list = []
     machine_filename = 'machine_setting.csv'
     machine_list = machine_set(machine_list, machine_filename)
+    # print(len(machine_list))
+    # for mc in machine_list:
+    #     for i in range (len(mc.processing)):
+    #         print(f"Machine name:{mc.machine_name}, Machine type:{mc.processing[i].typ}, Machine ID:{mc.processing[i].ID}, , Machine time:{mc.processing[i].time}")
+    # job_list = []
+    # job_filename = 'data.csv'
+    # job_list = job_chromosome_set(job_list, job_filename, machine_list)
+    # print(machine_list[18].processing[0].typ)
+    print(len(machine_list))
     
-    job_list = []
-    job_filename = 'data.csv'
-    job_list = job_set(job_list, job_filename)
-    print(len(job_list))
-    print(job_list[6].order)
